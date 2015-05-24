29e9b471d151e621955a962d6a9b41e6d70d07e2
Andrew Monteith <andrew.monteith1@gmail.com>
2015-05-24 23:09:25 +1000

    login test added for new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
+	before_action :authenticate_user!
+
 	def index
 		@products = Product.all
 	end
 	def new
-		before_action :authenticate_user!
 		@product = Product.new
-
 	end
 	def create
 		@product = Product.new(params.require(:product).permit(:cost,:name,:price,:image_id,:qty,:restock_time,:description))
