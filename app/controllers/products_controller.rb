b090511bb29bf7474bb4af6beddfde63db676f4d
Andrew Monteith <andrew.monteith1@gmail.com>
2015-05-24 23:23:56 +1000

    removed requirement for authentication from viewing products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 	before_action :authenticate_user!
-
+	skip_before_action :authenticate_user, only:[:index,:show]
 	def index
 		@products = Product.all
 	end
