499e53a49487824710171e0870b0ef88073cc38f
Andrew Monteith <andrew.monteith1@gmail.com>
2015-05-25 21:41:59 +1000

    Fixed logout issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, :images
 
+
   get 'welcome/index'
   root 'welcome#index'
+  get 'sign_out', to: 'users#sign_out_all_scopes'
 
 end
